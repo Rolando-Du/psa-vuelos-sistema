0b0c8679c8b71923c82ad43fe198d56e3f710f91
Rolando Duarte <rolandoduarte83@gmail.com>
2025-12-18 12:50:06 -0300

    Add backend date validation

--- backend/src/models/Flight.js
+++ backend/src/models/Flight.js
@@ -1,7 +1,21 @@
 import mongoose from 'mongoose';
 
 const flightSchema = new mongoose.Schema({
-    fecha: { type: String, required: true },
+    fecha: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                // Converto el string 'YYYY-MM-DD' a un objeto Date
+                const fechaIngresada = new Date(v + 'T00:00:00');
+                const hoy = new Date();
+                hoy.setHours(0, 0, 0, 0); // Pongo 'hoy' al inicio del día para comparar solo fechas
+                
+                return fechaIngresada >= hoy;
+            },
+            message: props => `La fecha ${props.value} no puede ser una fecha pasada.`
+        }
+    },
     hora: { type: String, required: true },
     matricula: { type: String, required: true },
     tipoAeronave: { type: String, required: true },
