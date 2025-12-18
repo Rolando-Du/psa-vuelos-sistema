1ec68f33fbd7947ed0430aa80d568207e4b9c102
Rolando Duarte <rolandoduarte83@gmail.com>
2025-12-18 13:50:12 -0300

    actualizaciones de funcionalidades nuevas

--- backend/src/models/Flight.js
+++ backend/src/models/Flight.js
@@ -1,41 +1,32 @@
 import mongoose from 'mongoose';
 
 const flightSchema = new mongoose.Schema({
-    fecha: { 
-        type: String, 
-        required: true,
-        validate: {
-            validator: function(v) {
-                // Converto el string 'YYYY-MM-DD' a un objeto Date
-                const fechaIngresada = new Date(v + 'T00:00:00');
-                const hoy = new Date();
-                hoy.setHours(0, 0, 0, 0); // Pongo 'hoy' al inicio del día para comparar solo fechas
-                
-                return fechaIngresada >= hoy;
-            },
-            message: props => `La fecha ${props.value} no puede ser una fecha pasada.`
-        }
-    },
+    fecha: { type: String, required: true },
     hora: { type: String, required: true },
     matricula: { type: String, required: true },
     tipoAeronave: { type: String, required: true },
     propietario: { type: String },
-    // CAMPOS SEPARADOS
     procedencia: { type: String, default: '' },
     destino: { type: String, default: '' },
     tipoMovimiento: { type: String, enum: ['ARRIBO', 'PARTIDA'], required: true },
     
-    apellidoNombre: { type: String, required: true },
-    tipoDni: { type: String, default: 'DNI' },
-    nroDni: { type: String, required: true },
-    tripPax: { type: String, enum: ['T', 'P'], default: 'T' },
-    nacionalidad: { type: String, default: 'ARG' },
-    equipajeMano: { type: Number, default: 0 },
-    equipajeBodega: { type: Number, default: 0 },
+    // Lista de personas
+    personas: [{
+        apellidoNombre: { type: String, required: true },
+        tipoDni: { type: String, default: 'DNI' },
+        nroDni: { type: String, required: true },
+        tripPax: { type: String, enum: ['T', 'P'], default: 'T' },
+        nacionalidad: { type: String, default: 'ARG' },
+        equipajeMano: { type: Number, default: 0 },
+        equipajeBodega: { type: Number, default: 0 },
+    }],
+
     gradoOficial: { type: String, required: true },
     nombreOficial: { type: String, required: true },
     lupOficial: { type: String, required: true },
-    observaciones: { type: String, default: '' }
+    
+    // Este campo guarda herramientas, victorinox, etc.
+    observaciones: { type: String, default: '' } 
 }, { timestamps: true });
 
 export default mongoose.model('Flight', flightSchema);
