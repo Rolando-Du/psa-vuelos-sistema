bcb094747ba9d041c9d7033e384b9538b2f9df87
Rolando Duarte <rolandoduarte83@gmail.com>
2025-12-20 15:15:26 -0300

    actualizo reder axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // Usa tu URL de Render
     baseURL: import.meta.env.VITE_API_URL || 'https://skylog-api.onrender.com/api',
 });
 
-// INTERCEPTOR: Esto soluciona el error 401
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token'); // Recupera el token guardado al hacer login
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+// Este interceptor "pega" el token en CUALQUIER llamada que haga la app
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            // Verifica que sea 'Authorization' con A mayúscula
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+);
 
 export default api;
